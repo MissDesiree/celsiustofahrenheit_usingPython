b88a927b843e6bb7b89e6f78cf76f0b96a0bcb01
MissDesiree <142279292+MissDesiree@users.noreply.github.com>
2024-03-20 06:23:04 -0400

    Add files via upload

---
+++ ProgramAssign1_M01.py
@@ -0,0 +1,7 @@
+def celsius_to_fahrenheit():
+    celsius = float(input("Enter temperature in Celsius: "))
+    fahrenheit = (9/5)*celsius + 32
+    print(f"The temperature in Fahrenheit is: {fahrenheit}")
+
+celsius_to_fahrenheit()
+
